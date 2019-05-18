f6c1f1d9c98787d44e20bda37d2b751cf6d3cc2c
陳家緯 <esawei5190@gmail.com>
2019-05-18 13:17:02 +0800

    extract interface

--- RsaSecureTokenTests/AuthenticationServiceTests.cs
+++ RsaSecureTokenTests/AuthenticationServiceTests.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NUnit.Framework;
-using RsaSecureToken;
-using Assert = NUnit.Framework.Assert;
+﻿using NUnit.Framework;
 
 namespace RsaSecureToken.Tests
 {
@@ -14,14 +6,35 @@ namespace RsaSecureToken.Tests
     public class AuthenticationServiceTests
     {
         [Test()]
-        public void IsValidTest()
+        public void IsValid()
         {
-            var target = new AuthenticationService();
+            var target = new AuthenticationService(new StubProfileDao(), new StubRsaTokenDao());
 
             var actual = target.IsValid("joey", "91000000");
 
             //always failed
             Assert.IsTrue(actual);                       
+        }
+    }
+
+    internal class StubProfileDao : IProfile
+    {
+        public string GetPassword(string account)
+        {
+            if (account=="joey")
+            {
+                return "91";
+            }
+
+            return "";
+        }
+    }
+
+    internal class StubRsaTokenDao : IToken
+    {
+        public string GetRandom(string account)
+        {
+            return "000000";
         }
     }
 }
